35b856f5082bc2f9aec807302949c7d1ed0a37e2
Pulpi79 <pulpi@openmail.cc>
2016-02-25 16:01:48 +0100

    ffd

--- WindowsFormsApplication16/Buchung.cs
+++ WindowsFormsApplication16/Buchung.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication16
 {
-    class Buchung
+    public class Buchung
     {
+        public string Betreff { get; set; }
+        public DateTime Datum { get; set; }
+
+        public string BuchungsTyp { get; set; }
+
+        public double Betrag { get; set; }
+
+        public string Vorzeichen { get; set; }
     }
 }
