083e42cf892e9d5ebde6eca03babb729107f806f
Pulpi79 <pulpi@openmail.cc>
2016-02-25 16:03:12 +0100

    dfg

--- WindowsFormsApplication16/Form1.cs
+++ WindowsFormsApplication16/Form1.cs
@@ -128,18 +128,34 @@ namespace WindowsFormsApplication16
         {
             var betreff = string.Empty;
             List<string> listToParse = new List<string>();
-            dictionary.TryGetValue("Buchung", out listToParse);
+            dictionary.TryGetValue("Buchung 1", out listToParse);
+            Buchung buchung = new Buchung();
+            Buchungen buchungen = new Buchungen();
 
-            string firstChar = listToParse[0].ToString().Substring(0, 1);
-            string getTextFromFirstLine = Regex.Match(listToParse[0], @"([A-Z])\w+").Value;
-            var resultString = Regex.Match(listToParse[0], @"\d+\,\d+").Value;
+            //string firstChar = listToParse[0].ToString().Substring(0, 1);
+            //string getTextFromFirstLine = Regex.Match(listToParse[0], @"([A-Z])\w+").Value;
+            //var resultString = Regex.Match(listToParse[0], @"\d+\,\d+").Value;
 
-            var datum = Regex.Match(listToParse[7], @"\d{2}\.\d{2}").Value;
+            //var datum = Regex.Match(listToParse[7], @"\d{2}\.\d{2}").Value;
+
+            //for (int i = 1; i <= 6; i++)
+            //{
+            //    betreff += listToParse[i].ToString();
+            //}
+
+            buchung.Vorzeichen = listToParse[0].ToString().Substring(0, 1);
+            buchung.BuchungsTyp = Regex.Match(listToParse[0], @"([A-Z])\w+").Value;
+            buchung.Betrag = Convert.ToDouble(Regex.Match(listToParse[0], @"\d+\,\d+").Value);
+
+            buchung.Datum = Convert.ToDateTime(Regex.Match(listToParse[7], @"\d{2}\.\d{2}").Value);
 
             for (int i = 1; i <= 6; i++)
             {
                 betreff += listToParse[i].ToString();
             }
+            buchung.Betreff = betreff;
+            buchungen.Add(buchung);
+            
 
         }
     }
