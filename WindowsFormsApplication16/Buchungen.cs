77d3ce36bad1941f1d81054b558919c71f27cbf4
Pulpi79 <pulpi@openmail.cc>
2016-02-25 16:04:38 +0100

    tttt

--- WindowsFormsApplication16/Buchungen.cs
+++ WindowsFormsApplication16/Buchungen.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication16
 {
-    class Buchungen
+    public class Buchungen : System.Collections.CollectionBase
     {
+        public void Add(Buchung buchung)
+        {
+            List.Add(buchung);
+        }
+
+        public void Remove(int index)
+        {
+            if (index > Count - 1 || index < 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Index not valid!");
+            }
+            else
+            {
+                List.RemoveAt(index);
+            }
+        }
+
+        public Buchung Item(int Index)
+        {
+            // The appropriate item is retrieved from the List object and
+            // explicitly cast to the Widget type, then returned to the 
+            // caller.
+            return (Buchung)List[Index];
+        }
     }
 }
